63560746be59e9cea66cd959708b18aec9f96478
Graham Walker <gwalker256@gmail.com>
2021-11-23 00:15:14 +0000

    loops7

--- activities.js
+++ activities.js
@@ -330,7 +330,26 @@
     
 // console.log(`The mutual followers are ${mufos}.`);
 
-// loops 7 not done
+// loops 7 
+
+// for (let num1 = 0; num1 < 10; num1++) {
+//     console.log(num1);
+//   };
+
+
+//   let num2 = 0;
+//   let num3 = 10;
+//   while (num2 < num3) {
+//         console.log(num2);
+//         num2++;
+
+//   }
+
+//   let num4 = 0;
+//   do {
+//     console.log(num4);
+//     num4++;
+//   } while (num4<10);
 
 // functions activity 1
 
--- test.js
+++ test.js
@@ -1,6 +1,20 @@
 // console.log("Javascript is tricky");
 
-const nums = [1, 2, 4, 5];
-console.log (nums)
-nums.splice(2, 0, 3);
-console.log(nums);
+for (let num1 = 0; num1 < 10; num1++) {
+    console.log(num1);
+  };
+
+
+  let num2 = 0;
+  let num3 = 10;
+  while (num2 < num3) {
+        console.log(num2);
+        num2++;
+
+  }
+
+  let num4 = 0;
+  do {
+    console.log(num4);
+    num4++;
+  } while (num4<10);
